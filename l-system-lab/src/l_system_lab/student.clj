88a8ee82635d55118d08c7328ac546f169cd49c8
Adeeb Ahmed <aahmed22@hawk.iit.edu>
2014-04-08 22:04:02 -0500

    ACTUAL FIRST  LSYS SUBMISSION

--- l-system-lab/src/l_system_lab/student.clj
+++ l-system-lab/src/l_system_lab/student.clj
@@ -5,25 +5,142 @@
   [v]
   (filter #(not= [] %) v))
 
-;;
-(defn get-xy-scale
-  "Get the scaling factor for the coordinates."
-  ([v]  {:scale 1 :min-x 1 :min-y 1})
-  ([v min-x min-y max-x max-y]
-     {:scale 1 :min-x 1 :min-y 1}))
+(defn scale-turtle-one
+  [v]
+ (let [vector1 (v 0)
+       [x1 y1 x2 y2] (rest vector1)
+       out[]
+       xvec (into out [x1 x2])
+       yvec (into out [y1 y2])
+       xmin (apply min xvec)
+       ymin (apply min yvec)
+       xmax (apply max xvec)
+       ymax (apply max yvec)
+       xrange (- xmax xmin)
+       yrange (- ymax ymin)
+       scale (:scale (get-xy-scale v))]
+
+  (into [] [[:line (double (-> x1 (- xmin) (* scale) (+ 10)))
+            (double (-> y1 (- ymin) (* scale) (+ 10)))
+             (double (-> x2 (- xmin) (* scale) (+ 10)))
+             (double (-> y2 (- ymin) (* scale) (+ 10)))
+           ]]
+   )
+ )
+)
+
+(defn scale-turtle-two
+  [v]
+ (let [vector1 (v 0)
+       vector2 (v 1)
+       [x1 y1 x2 y2] (rest vector1)
+       [x3 y3 x4 y4] (rest vector2)
+       out[]
+
+       xvec (into out [x1 x2 x3 x4])
+       yvec (into out [y1 y2 y3 y4])
+       xmin (apply min xvec)
+       ymin (apply min yvec)
+       xmax (apply max xvec)
+       ymax (apply max yvec)
+       xrange (- xmax xmin)
+       yrange (- ymax ymin)
+       scale (:scale (get-xy-scale v))]
+
+  (conj [] [[:line (double (-> x1 (- xmin) (* scale) (+ 10)))
+            (double (-> y1 (- ymin) (* scale) (+ 10)))
+             (double (-> x2 (- xmin) (* scale) (+ 10)))
+             (double (-> y2 (- ymin) (* scale) (+ 10)))]
+
+            [:line (double (-> x3 (- xmin) (* scale) (+ 10)))
+             (double (-> x3 (- xmin) (* scale) (+ 10)))
+             (double (-> x3 (- xmin) (* scale) (+ 10)))
+             (double (-> x3 (- xmin) (* scale) (+ 10)))]]
+   )
+ )
+)
 
 
-;; You will need to rewrite this.  This code is just for show.
 (defn scale-turtle
-  "Normalizes a list of [:line ... ] vectors."
-  ([v] (scale-turtle v (get-xy-scale (remove-empties v)) []))
-  ([v scale out]
-     v ;; For initial, just return the original.
-     ))
+  [v]
+   (if (= (count v) 1)
+     (scale-turtle-one v)
+   (if (= (count v) 2)
+     (scale-turtle-two v)))
+)
+
 
 (defn transform
-  [init-pat rules]
-  init-pat ;; Make it do something here!
+  "Creates fractal transformation pattern"
+  [init-pat rules] ;; passes in a vector (init-pat) and hashmap (rules)
+  (loop [v init-pat  ;; intitialze vector with the initialzation patten passed in by the user
+         out[]]
+    (if (empty? rules) init-pat
+    (if (empty? v) out ;;outputs empty vector
+      (if (identical? (first v) (keys rules))  ;; if first ele of vector is equal to the keys within the rules hashmap
+         (recur (rest v) (into out (rules (first (key rules)))))
+         (recur (rest v) (into out (rest v)))))))
 )
+
+(defn get-xy-scale-one
+  "Get the scaling factor for the coordinates."
+  [v]
+  (let [vector1 (v 0)
+        [x1 y1 x2 y2] (rest vector1)
+        out[]
+       xvec (into out [x1 x2])
+       yvec (into out [y1 y2])
+       xmin (apply min xvec)
+       xmax (apply max xvec)
+       ymin (apply min yvec)
+       ymax (apply max yvec)
+       xrange (- xmax xmin)
+       yrange (- ymax ymin)]
+
+       (cond (< xrange yrange)
+         {:scale (double (/ 480 yrange)), :min-x xmin, :min-y ymin}
+         :else  {:scale (double (/ 480 xrange)), :min-x xmin, :min-y ymin})
+      )
+   )
+
+
+
+(defn get-xy-scale-two
+  "Get the scaling factor for the coordinates."
+  [v]
+  (let [vector1 (v 0)
+        vector2 (v 1)
+        [x1 y1 x2 y2] (rest vector1)
+        [x3 y3 x4 y4] (rest vector2)
+        out []
+       xvec (into out[x1 x2 x3 x4])
+       yvec (into out[y1 y2 y3 y4])
+       xmin (apply min xvec)
+       xmax (apply max xvec)
+       ymin (apply min yvec)
+       ymax (apply max yvec)
+       xrange (- xmax xmin)
+       yrange (- ymax ymin)]
+
+       (cond (< xrange yrange)
+         {:scale (double (/ 480 yrange)), :min-x xmin, :min-y ymin}
+         :else  {:scale (double (/ 480 xrange)), :min-x xmin, :min-y ymin})
+
+    )
+  )
+
+(defn get-xy-scale
+  [v]
+   (if (= (count v) 1)
+     (get-xy-scale-one v)
+   (if (= (count v) 2)
+     (get-xy-scale-two v))) ;else
+)
+
+;; (defn get-xy-scale
+;;   [v]
+;;   (cond (= (count v) 1)  (get-xy-scale-one v)
+;;         :else (get-xy-scale-two v)))
+
 
 ;; # Some fractals to start out with.  Add some of your own!
